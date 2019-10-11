11fadad30c155555778c65ce751bbb67eb6a9fa8
Javii03 <55969273+Javii03@users.noreply.github.com>
2019-10-11 11:27:47 -0700

    Update fixationGraph.js

--- fixationGraph.js
+++ fixationGraph.js
@@ -5,7 +5,7 @@ var bar1 = d3.select("svg")
              .attr('transform', 'translate(0,0)')
              .append("rect")
              .attr('width', '1280')
-             .attr('height', '500')
+             .attr('height', '1024')
              .attr('fill', 'rgba(0,0,0,0)')
              .attr('stroke', '#2378ae')
              .attr('stroke-width', '3')
@@ -13,9 +13,11 @@ var bar1 = d3.select("svg")
 
 
     var width = 1280;
+    var height = 1024;
 
 
 var data = [0, 150, 200, 250, 1280];
+var data1 = [0, 150, 200, 250, 1024];
 
 // Append SVG 
 var svg = d3.select("body").select("svg")
@@ -24,12 +26,25 @@ var svg = d3.select("body").select("svg")
 // Create scale
 var scale = d3.scaleLinear()
               .domain([d3.min(data), d3.max(data)])
-              .range([0, width - 600]);
+              .range([0, width - 0]);
+
+var scale1 = d3.scaleLinear()
+               .domain([d3.min(data1), d3.max(data1)])
+               .range([height/2, 0]);
+              
 
 // Add scales to axis
 var x_axis = d3.axisBottom()
                .scale(scale);
 
+var y_axis = d3.axisRight()
+                .scale(scale);
 //Append group and insert axis
 svg.append("g")
    .call(x_axis);
+  
+svg.append("g")
+    .attr("transform", "translate(10,10")
+   .call(y_axis);
+
+
